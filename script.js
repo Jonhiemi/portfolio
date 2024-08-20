93ba542dc20420e40045de6ca64b4608e7957da7
Jonathan Moyano <103074392+Jonhiemi@users.noreply.github.com>
2024-08-20 20:24:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Selecciona el botón de "Ver más" en la tarjeta principal
+    const showInfoButton = document.querySelector('.show-info-btn');
+    const popupCard = document.getElementById('popup-card');
+    const closePopupButton = document.querySelector('.close-popup-btn');
+
+    // Mostrar la tarjeta emergente cuando se hace clic en "Ver más"
+    showInfoButton.addEventListener('click', function () {
+        popupCard.style.display = 'block';
+    });
+
+    // Cerrar la tarjeta emergente cuando se hace clic en "Cerrar"
+    closePopupButton.addEventListener('click', function () {
+        popupCard.style.display = 'none';
+    });
+
+    // Cerrar la tarjeta emergente si el usuario hace clic fuera del contenido de la tarjeta
+    window.addEventListener('click', function (event) {
+        if (event.target === popupCard) {
+            popupCard.style.display = 'none';
+        }
+    });
+});
+
 document.getElementById('contacto').addEventListener('submit', function(event) {
     event.preventDefault();
     const nombre = document.getElementById('nombre').value;
@@ -30,5 +54,3 @@ document.getElementById('contacto').addEventListener('submit', function(event) {
         console.error('Error al enviar el formulario:', error);
     });
 });
-
-    
